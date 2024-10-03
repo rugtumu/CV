b6b0179911246be3168d7287311893d8db4649d2
rugtumu <umutgur7@gmail.com>
2024-10-03 14:43:44 +0300

    Speed Insights added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { GlobeIcon, MailIcon, PhoneIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { RESUME_DATA } from "@/data/resume-data";
 import { ProjectCard } from "@/components/project-card";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
