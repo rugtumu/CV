bcbf47d73d4588007072fa5622183ed9baaa583f
Umut <umutgur7@gmail.com>
2025-04-18 21:42:16 +0300

    404 page updated

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,9 @@
 export default function NotFound() {
     return (
       <div className="flex flex-col items-center justify-center h-screen bg-white text-gray-800 p-8">
-        <h1 className="text-2xl font-semibold">Bu sayfa henüz inşa edilmedi.</h1>
+        <h1 className="text-xl font-semibold">Aradığınız siteye şu anda ulaşılamıyor.</h1>
+        <h1 className="text-xl font-semibold">Lütfen daha sonra
+        tekrar deneyiniz.</h1>
       </div>
     );
   }
